6e075a6479d30212282b6fabd83935200bdcabb9
ball1326 <122956594+ball1326@users.noreply.github.com>
2023-04-19 21:29:18 +0700

    first commit

---
+++ Object Repository/Page_Form2/form_Text1            Text2            Text_4386c5.rs
@@ -0,0 +1,452 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>form_Text1            Text2            Text_4386c5</name>
+   <tag></tag>
+   <elementGuidId>4718e361-ff11-4ea0-8dfd-d22b3745402f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>form</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//form</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>form</value>
+      <webElementGuid>384bda40-e97d-44a4-98de-d4bc960b8a92</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      Text1:
+      
+      Text2:
+      
+      Text3:
+      
+      Text4:
+      
+      Text5:
+      
+      Text6:
+      
+      Text7:
+      
+      Text8:
+      
+      Text9:
+      
+      Text10:
+       
+        
+        Checkbox1
+        
+        Checkbox2      
+        
+        Checkbox3
+        
+        Checkbox4   
+        
+        Checkbox5
+        
+        Checkbox6   
+        
+        Checkbox7
+        
+        Checkbox8         
+        
+        Checkbox9   
+        
+        Checkbox10
+        
+        Radio1
+        
+        Radio2  
+        
+        Radio3
+        
+        Radio4          
+        
+        Radio5
+        
+        Radio6      
+        
+        Radio7    
+        
+        Radio8    
+        
+        Radio9    
+        
+        Radio10
+        Choose a option1:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option2:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option3:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option4:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option5:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option6:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option7:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option8:
+        
+          value1
+          value2
+          value3
+          value4
+         
+        Choose a option9:
+        
+          value1
+          value2
+          value3
+          value4
+          
+        Choose a option10:
+        
+          value1
+          value2
+          value3
+          value4
+                                                   
+      </value>
+      <webElementGuid>aab59a46-c16c-468b-a85d-8572f75af6e9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/form[1]</value>
+      <webElementGuid>cc0ae5e6-42a5-4bfd-bdc5-78f4a42e1d99</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form</value>
+      <webElementGuid>99399baf-ff81-4a13-837a-3bcc81241be6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//form[(text() = '
+      Text1:
+      
+      Text2:
+      
+      Text3:
+      
+      Text4:
+      
+      Text5:
+      
+      Text6:
+      
+      Text7:
+      
+      Text8:
+      
+      Text9:
+      
+      Text10:
+       
+        
+        Checkbox1
+        
+        Checkbox2      
+        
+        Checkbox3
+        
+        Checkbox4   
+        
+        Checkbox5
+        
+        Checkbox6   
+        
+        Checkbox7
+        
+        Checkbox8         
+        
+        Checkbox9   
+        
+        Checkbox10
+        
+        Radio1
+        
+        Radio2  
+        
+        Radio3
+        
+        Radio4          
+        
+        Radio5
+        
+        Radio6      
+        
+        Radio7    
+        
+        Radio8    
+        
+        Radio9    
+        
+        Radio10
+        Choose a option1:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option2:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option3:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option4:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option5:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option6:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option7:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option8:
+        
+          value1
+          value2
+          value3
+          value4
+         
+        Choose a option9:
+        
+          value1
+          value2
+          value3
+          value4
+          
+        Choose a option10:
+        
+          value1
+          value2
+          value3
+          value4
+                                                   
+      ' or . = '
+      Text1:
+      
+      Text2:
+      
+      Text3:
+      
+      Text4:
+      
+      Text5:
+      
+      Text6:
+      
+      Text7:
+      
+      Text8:
+      
+      Text9:
+      
+      Text10:
+       
+        
+        Checkbox1
+        
+        Checkbox2      
+        
+        Checkbox3
+        
+        Checkbox4   
+        
+        Checkbox5
+        
+        Checkbox6   
+        
+        Checkbox7
+        
+        Checkbox8         
+        
+        Checkbox9   
+        
+        Checkbox10
+        
+        Radio1
+        
+        Radio2  
+        
+        Radio3
+        
+        Radio4          
+        
+        Radio5
+        
+        Radio6      
+        
+        Radio7    
+        
+        Radio8    
+        
+        Radio9    
+        
+        Radio10
+        Choose a option1:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option2:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option3:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option4:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option5:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option6:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option7:
+        
+          value1
+          value2
+          value3
+          value4
+        
+        Choose a option8:
+        
+          value1
+          value2
+          value3
+          value4
+         
+        Choose a option9:
+        
+          value1
+          value2
+          value3
+          value4
+          
+        Choose a option10:
+        
+          value1
+          value2
+          value3
+          value4
+                                                   
+      ')]</value>
+      <webElementGuid>8b29b006-e719-45e2-9134-a15a7021c582</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/ts2001.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts2001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f29b5ebe-0965-4255-8bca-450e0d0d5701</testSuiteGuid>
+   <testCaseLink>
+      <guid>19ec50b2-d12f-4d7b-9ba9-9a77d7b19fa4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>51c5901c-7310-4e37-83f7-14908c34452d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test case2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
